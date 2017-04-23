b3206273d79fb800961703405768638b61a792ce
saintchubs <aearnshaw@hotmail.co.uk>
2017-04-23 22:33:39 +0100

    Adding contracts project

---
+++ Stc.AppTemplate/Core/Stc.AppTemplate.Core.Contracts/SampleContract.cs
@@ -0,0 +1,10 @@
+﻿using Newtonsoft.Json;
+
+namespace Stc.AppTemplate.Core.Contracts
+{
+    public class SampleContract
+    {
+        [JsonProperty]
+        public string Name { get; set; }
+    }
+}
