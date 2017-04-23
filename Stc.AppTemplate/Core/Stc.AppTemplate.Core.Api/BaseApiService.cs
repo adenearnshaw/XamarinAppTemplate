30cc9cf31c5db6b240fbb55019fe141b3c61fa23
saintchubs <aearnshaw@hotmail.co.uk>
2017-04-23 19:18:41 +0100

    Adding Api layer

---
+++ Stc.AppTemplate/Core/Stc.AppTemplate.Core.Api/BaseApiService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Stc.AppTemplate.Core.Api
+{
+    public class BaseApiService
+    {
+        private readonly string _serviceBaseUrl;
+
+        public BaseApiService(string serviceBaseUrl)
+        {
+            _serviceBaseUrl = serviceBaseUrl;
+        }
+
+        protected async Task<HttpResponseMessage> SendUnauthorisedRequestAsync(HttpRequestMessage message)
+        {
+            var client = BuildClient();
+            var response = await client.SendAsync(message);
+            return response;
+        }
+
+        private HttpClient BuildClient()
+        {
+            var client = new HttpClient
+            {
+                BaseAddress = new Uri(_serviceBaseUrl)
+            };
+            return client;
+        }
+    }
+}
