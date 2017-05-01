21bb9bffcdf65fe28f971a20a180460f6261fcbf
Aden Earnshaw <Aden.Earnshaw@webuyanycar.com>
2017-05-01 22:54:17 +0100

    Adding UnitTest shell projects to template

---
+++ Stc.AppTemplate/Tests/Stc.AppTemplate.Core.ApiTests/TestClass.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stc.AppTemplate.Core.ApiTests
+{
+    [TestFixture]
+    public class TestClass
+    {
+        [Test]
+        public void TestMethod()
+        {
+            // TODO: Add your test code here
+            Assert.Pass("Your first passing test");
+        }
+    }
+}
